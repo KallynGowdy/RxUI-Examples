a5beabfcdc1594850d0a1abaa87d3ffce727bfaa
Kallyn Gowdy <kallyngowdy@gmail.com>
2016-06-21 01:32:25 -0400

    Add Routing, Clear All, and Remaining Count

--- todomvc/react/app/components/app.tsx
+++ todomvc/react/app/components/app.tsx
@@ -2,18 +2,27 @@ import * as React from 'react';
 import {TodoViewModel, LocalTodoStorage, Todo} from 'rxui-example-core';
 import {TodoComponent} from "./todo";
 import {Subscription} from "rxjs/Rx";
+import {Link} from "react-router";
+import * as classNames from "classnames";
 
 export interface AppState {
     todos: Todo[];
     newTodo: string;
     allCompleted: boolean;
+    canClearComplete: boolean;
+    incompleteCount: number;
+    remainingText: string;
 }
 
-export class AppComponent extends React.Component<{}, AppState> {
+export interface AppParams {
+    params: { status: string };
+}
+
+export class AppComponent extends React.Component<AppParams, AppState> {
     viewModel: TodoViewModel;
     subscriptions: Subscription[];
-    constructor() {
-        super();
+    constructor(params?: AppParams) {
+        super(params);
         this.viewModel = new TodoViewModel(new LocalTodoStorage());
         this.subscriptions = [];
         this.state = this.getViewModelState();
@@ -21,13 +30,34 @@ export class AppComponent extends React.Component<{}, AppState> {
 
     componentDidMount() {
         this.subscriptions = [
-            this.viewModel.whenAnyValue(vm => vm.visibleTodos, vm => vm.newTodo.title, (todos, title) => ({ todos, title }))
+            this.viewModel.whenAnyValue(vm => vm.visibleTodos,
+                vm => vm.newTodo.title,
+                vm => vm.areAllTodosComplete,
+                vm => vm.remainingText,
+                (todos, title) => ({ todos, title }))
+                .combineLatest(this.viewModel.completedTodos.whenAnyValue(v => v.length))
+                .combineLatest(this.viewModel.incompleteTodos.whenAnyValue(v => v.length))
                 .map(todos => this.getViewModelState())
                 .do(state => this.setState(state))
                 .subscribe(),
-
             this.viewModel.loadTodos.invoke().subscribe()
         ];
+    }
+
+    componentWillReceiveProps(props: AppParams) {
+        if (props.params.status) {
+            switch (props.params.status) {
+                case "active":
+                    this.viewModel.status = "incomplete";
+                    break;
+                case "completed":
+                    this.viewModel.status = "complete";
+                    break;
+                default:
+                    this.viewModel.status = "all";
+                    break;
+            }
+        }
     }
 
     componentWillUnmount() {
@@ -40,7 +70,10 @@ export class AppComponent extends React.Component<{}, AppState> {
         return {
             todos: this.viewModel.visibleTodos,
             newTodo: this.viewModel.newTodo.title,
-            allCompleted: this.viewModel.areAllTodosComplete
+            allCompleted: this.viewModel.areAllTodosComplete,
+            canClearComplete: this.viewModel.completedTodos.length > 0,
+            incompleteCount: this.viewModel.incompleteTodos.length,
+            remainingText: this.viewModel.remainingText
         }
     }
 
@@ -55,6 +88,10 @@ export class AppComponent extends React.Component<{}, AppState> {
 
     handleToggleAll(change: any) {
         this.viewModel.toggleAllComplete.invoke().subscribe();
+    }
+
+    handleClearCompleted() {
+        this.viewModel.clearComplete.invoke().subscribe();
     }
 
     render() {
@@ -77,20 +114,23 @@ export class AppComponent extends React.Component<{}, AppState> {
                 </section>
                 <footer className="footer" >
                     <span className="todo-count">
-                        <strong></strong>
+                        <strong>{this.state.incompleteCount}</strong>
+                        {" " + this.state.remainingText}
                     </span>
                     <ul className="filters">
                         <li>
-                            <a>All</a>
+                            <Link to={"/all"} className={classNames({selected: this.props.params.status === "all"})}>All</Link>
                         </li>
                         <li>
-                            <a>Active</a>
+                            <Link to={"/active"} className={classNames({selected: this.props.params.status === "active"})}>Active</Link>
                         </li>
                         <li>
-                            <a>Completed</a>
+                            <Link to={"/completed"} className={classNames({selected: this.props.params.status === "completed"})}>Completed</Link>
                         </li>
                     </ul>
-                    <button className="clear-completed">Clear completed</button>
+                    {(() => this.state.canClearComplete ?
+                        <button className="clear-completed" onClick={this.handleClearCompleted.bind(this) }>Clear completed</button>
+                        : "")() }
                 </footer>
             </section>
         );

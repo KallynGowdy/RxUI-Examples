051480ef539b7e42f5a42932c70ede1fe8eba700
Kallyn Gowdy <kallyngowdy@gmail.com>
2016-06-20 23:43:43 -0400

    Improve Style Support and Integrate More Features

--- todomvc/react/app/components/app.tsx
+++ todomvc/react/app/components/app.tsx
@@ -1,68 +1,97 @@
 import * as React from 'react';
 import {TodoViewModel, LocalTodoStorage, Todo} from 'rxui-example-core';
 import {TodoComponent} from "./todo";
+import {Subscription} from "rxjs/Rx";
 
 export interface AppState {
-    todos: Todo[]
+    todos: Todo[];
+    newTodo: string;
+    allCompleted: boolean;
 }
 
 export class AppComponent extends React.Component<{}, AppState> {
     viewModel: TodoViewModel;
-
+    subscriptions: Subscription[];
     constructor() {
         super();
         this.viewModel = new TodoViewModel(new LocalTodoStorage());
+        this.subscriptions = [];
         this.state = this.getViewModelState();
-        this.viewModel.whenAnyValue(vm => vm.visibleTodos)
-            .map(todos => this.getViewModelState())
-            .do(state => this.setState(state))
-            .subscribe();
+    }
 
-        this.viewModel.loadTodos.invoke().subscribe();
+    componentDidMount() {
+        this.subscriptions = [
+            this.viewModel.whenAnyValue(vm => vm.visibleTodos, vm => vm.newTodo.title, (todos, title) => ({ todos, title }))
+                .map(todos => this.getViewModelState())
+                .do(state => this.setState(state))
+                .subscribe(),
+
+            this.viewModel.loadTodos.invoke().subscribe()
+        ];
+    }
+
+    componentWillUnmount() {
+        this.subscriptions.forEach(sub => {
+            sub.unsubscribe();
+        });
     }
 
     getViewModelState() {
         return {
-            todos: this.viewModel.visibleTodos
+            todos: this.viewModel.visibleTodos,
+            newTodo: this.viewModel.newTodo.title,
+            allCompleted: this.viewModel.areAllTodosComplete
         }
     }
 
+    handleAddTodo(e: any) {
+        e.preventDefault();
+        this.viewModel.addTodo.invoke().subscribe();
+    }
+
+    handleNewTodoChange(change: any) {
+        this.viewModel.newTodo.title = change.target.value;
+    }
+
+    handleToggleAll(change: any) {
+        this.viewModel.toggleAllComplete.invoke().subscribe();
+    }
+
     render() {
+        var todos = this.state.todos.map(t => (<TodoComponent key={t.title} todo={t} viewModel={this.viewModel}/>));
 
-        var todos = this.state.todos.map(t => (<TodoComponent todo={t} viewModel={this.viewModel}/>));
-
-         return (
-        <section id="todoapp">
-            <header id="header">
-                <h1>todos</h1>
-                <form id="todo-form">
-                    <input id="new-todo" placeholder="What needs to be done?" autofocus />
-                </form>
-            </header>
-            <section id="main">
-                <input id="toggle-all" type="checkbox"/>
-                <label for="toggle-all">Mark all as complete</label>
-                <ul id="todo-list">
-                    {todos}
-                </ul>
-            </section>
-            <footer id="footer" >
-                <span id="todo-count">
-                    <strong></strong>
-                </span>
-                <ul id="filters">
-                    <li>
-                        <a>All</a>
-                    </li>
-                    <li>
-                        <a>Active</a>
-                    </li>
-                    <li>
-                        <a>Completed</a>
-                    </li>
-                </ul>
-                <button id="clear-completed"> Clear completed</button>
-            </footer>
+        return (
+            <section className="todoapp">
+                <header className="header">
+                    <h1>todos</h1>
+                    <form className="todo-form" onSubmit={this.handleAddTodo.bind(this) }>
+                        <input className="new-todo" value={this.state.newTodo} onChange={this.handleNewTodoChange.bind(this) } placeholder="What needs to be done?" autofocus />
+                    </form>
+                </header>
+                <section className="main">
+                    <input id="toggle-all" className="toggle-all" checked={this.state.allCompleted} onChange={this.handleToggleAll.bind(this) } type="checkbox"/>
+                    <label htmlFor="toggle-all">Mark all as complete</label>
+                    <ul className="todo-list">
+                        {todos}
+                    </ul>
+                </section>
+                <footer className="footer" >
+                    <span className="todo-count">
+                        <strong></strong>
+                    </span>
+                    <ul className="filters">
+                        <li>
+                            <a>All</a>
+                        </li>
+                        <li>
+                            <a>Active</a>
+                        </li>
+                        <li>
+                            <a>Completed</a>
+                        </li>
+                    </ul>
+                    <button className="clear-completed">Clear completed</button>
+                </footer>
             </section>
         );
     }
--- todomvc/react/app/components/todo.tsx
+++ todomvc/react/app/components/todo.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
+import * as ReactDom from "react-dom";
 import {Todo, TodoViewModel} from "rxui-example-core";
+import * as classNames from "classnames";
+import {Subscription} from "rxjs/Rx";
 
 export interface TodoProps {
     todo: Todo;
@@ -12,9 +15,30 @@ export interface TodoState {
 }
 
 export class TodoComponent extends React.Component<TodoProps, TodoState> {
+    titleInput: HTMLInputElement;
+    subscriptions: Subscription[];
+    constructor(props?: TodoProps) {
+        super(props);
+        this.state = this.getTodoState();
 
-    constructor() {
-        super();
+    }
+
+    componentDidMount() {
+        this.subscriptions = [
+            this.props.todo.whenAnyValue(t => t.title, t => t.completed, (title, complete) => ({ title, complete }))
+                .combineLatest(this.props.viewModel.whenAnyValue(vm => vm.editedTodo))
+                .map(args => this.getTodoState())
+                .do(state => {
+                    this.setState(state);
+                })
+                .subscribe()
+        ];
+    }
+
+    componentWillUnmount() {
+        this.subscriptions.forEach(sub => {
+            sub.unsubscribe();
+        });
     }
 
     getTodoState() {
@@ -24,38 +48,56 @@ export class TodoComponent extends React.Component<TodoProps, TodoState> {
         };
     }
 
-    getInitialState() {
-        return this.getTodoState();
-    }
-
     handleToggle(change: any) {
-        this.props.todo.completed = change.target.value;
-        this.setState(this.getTodoState());
+        this.props.viewModel.toggleTodo.invoke(this.props.todo).subscribe();
     }
 
     handleTitleChange(change: any) {
         this.props.todo.title = change.target.value;
-        this.setState(this.getTodoState());
     }
 
-    handleSubmit(e) {
-        e.preventDefault();
-        this.props.viewModel.finishEditing.invoke().subscribe(() => {
-            this.setState(this.getTodoState());
+    handleKeyUp(e: React.KeyboardEvent) {
+        if (e.keyCode === 27) {
+            this.props.viewModel.undo.invoke().subscribe();
+        } else if (e.keyCode === 13) {
+            this.props.viewModel.finishEditing.invoke().subscribe();
+        }
+    }
+
+    handleBlur(e: React.SyntheticEvent) {
+        this.props.viewModel.finishEditing.invoke().subscribe();
+    }
+
+    handleDoubleClick(e: React.MouseEvent) {
+        this.props.viewModel.editTodo.invoke(this.props.todo).subscribe(() => {
+            setTimeout(() => {
+                if (this.titleInput !== null) {
+                    this.titleInput.focus();
+                }
+            }, 0);
         });
+    }
+
+    handleRemoveTodo(e: React.SyntheticEvent) {
+        e.preventDefault();
+        this.props.viewModel.deleteTodo.invoke(this.props.todo).subscribe();
     }
 
     render() {
         return (
-            <li>
+            <li className={classNames({ completed: this.state.completed, editing: this.props.todo === this.props.viewModel.editedTodo }) }>
                 <div className="view">
-                    <input className="toggle" type="checkbox" checked={this.state.completed} onChange={this.handleToggle} />
-                    <label dblclick="editTodo()">{this.state.title}</label>
-                    <button className="destroy" click="removeTodo()"></button>
+                    <input className="toggle" type="checkbox" checked={this.state.completed} onChange={this.handleToggle.bind(this) } />
+                    <label onDoubleClick={this.handleDoubleClick.bind(this) }>{this.state.title}</label>
+                    <button className="destroy" onClick={this.handleRemoveTodo.bind(this) }></button>
                 </div>
-                <form onSubmit={this.handleSubmit}>
-                    <input className="edit" value={this.state.title} onChange={this.handleTitleChange} keyup="revertEdits()" blur="saveEdits()" />
-                </form>
+                <input
+                    className="edit"
+                    ref={(ref) => this.titleInput = ref}
+                    value={this.state.title}
+                    onChange={this.handleTitleChange.bind(this) }
+                    onKeyUp={this.handleKeyUp.bind(this) }
+                    onBlur={this.handleBlur.bind(this) } />
             </li>
         );
     }
